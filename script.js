d5aa2d1c7b93c398e01af503d23f3119c34d1199
dana-wojtowicz <92703707+dana-wojtowicz@users.noreply.github.com>
2022-02-11 00:09:54 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+	let ourCode = {
+	codeName: "Turning Head Towards The Sound",
+	steps: [
+	"1. Begin",
+	"2.		",
+	"3. Begin",
+	"4.		",
+	"5. Begin",
+	"6.		",
+	"7. Begin",
+	"8.		",
+	"9. Begin",
+	"10.	"
+
+	]
+
+
+}
+
+
+
+
+// Logic of our code
+function getsound (arrr)
+{
+
+	
+	let soundFound= [];
+	console.log = "Working";
+		for (let degree=0; degree<360); {
+		if (soundFound) {
+
+			console.log="Sound found";
+
+			soundFound.push(1);
+		}
+	
+		else{
+
+
+			sound = degree+1;
+
+		}
+	
+}
+
+
+
+
+// Code from W3 about how to use fuctions
+let x = myFunction(4, 3);
+
+document.getElementById("demo").innerHTML = x;
+
+function myFunction(a, b) {
+return a + b;
+}
+
+console.log(x);
+}
